067857b05d3aac06d7f763cc1703e69517468d57
Bahloul <hitch95@protonmail.com>
2024-11-01 23:08:06 +0100

    add the test part. on the github actions

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sum = function (a, b) {
-    return a + b;
-};
-exports.default = sum;
--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
+import * as core from '@actions/core';
+
 const sum = (a: number, b: number) => {
-    return a + b;
+  return a + b;
 };
+
+async function run(): Promise<void> {
+  try {
+    const number1 = parseFloat(core.getInput('number1'));
+    const number2 = parseFloat(core.getInput('number2'));
+
+    const result = sum(number1, number2);
+
+    // Set the output for the action
+    core.setOutput('result', result.toString());
+  } catch (error: Error | unknown) {
+    core.setFailed((error as Error).message);
+  }
+}
+
+run();
 
 export default sum;
